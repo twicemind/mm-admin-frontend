da2a7c48515b4dddc72f4ab1892ef0cbbd952a48
Thomas.Herfort <thomas.herfort@br.de>
2026-04-18 22:53:31 +0200

    fix: configure ESLint and fix CI test configuration

--- karma.conf.js
+++ karma.conf.js
@@ -49,6 +49,8 @@ module.exports = function (config) {
         flags: [
           '--no-sandbox',
           '--disable-gpu',
+          '--disable-dev-shm-usage',
+          '--disable-software-rasterizer',
           '--disable-dev-shm-usage'
         ]
       }
